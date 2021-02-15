bee9159c7cec6e264b1aa5d28df9dc45ee43f1ea
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-02-15 12:25:55 +0100

    /

---
+++ db.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="79e72cde29d",
+  database="shoes_db"
+)
+
+print(mydb)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS jordan (id INT AUTO_INCREMENT PRIMARY KEY, img_src VARCHAR(255), name VARCHAR(255), type VARCHAR(255), nbr_color VARCHAR(255), price DECIMAL)")
+
+def insert_jordan(value_data):
+    sql = "INSERT INTO jordan (img_src, name, type, nbr_color, price) VALUES (%s, %s, %s, %s, %s)"
+    val = value_data
+    mycursor.executemany(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
---
+++ main.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+from db import insert_jordan
+
+my_data = []
+html_text = requests.get('https://www.nike.com/fr/w/hommes-jordan-chaussures-37eefznik1zy7ok').text
+soup = BeautifulSoup(html_text, 'lxml')
+shoes = soup.find_all('div', class_ = "product-card css-p5gpvn css-z5nr6i css-11ziap1 css-zk7jxt css-dpr2cn product-grid__card")
+
+for shoe in shoes:    
+    shoe_img_noscript = shoe.find('noscript')
+    shoe_img_src = shoe_img_noscript.find('img', class_ = 'css-1fxh5tw product-card__hero-image')['src']
+    shoe_name = shoe.find('div', class_ = "product-card__title").text
+    shoe_type = shoe.find('div', class_ = "product-card__subtitle").text
+    shoe_color = shoe.find('div', class_ = "product-card__product-count").text
+    shoe_price = float(shoe.find('div', class_ = "product-card__price").text[:-2].replace(',', '.'))
+    my_data.append((shoe_img_src, shoe_name, shoe_type, shoe_color, shoe_price))
+    # print(f'''
+    # Shoe image : {shoe_img_src},
+    # Shoe name : {shoe_name}, 
+    # Shoe type : {shoe_type}, 
+    # Shoe number of color : {shoe_color}, 
+    # Show price : {shoe_price}
+    # ''')
+
+print("shoes = ",my_data)
+
+insert_jordan(my_data)
